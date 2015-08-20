5442faa15c9e24d1658849a9858500dbee6913c0
inker610566 <inker610566@yahoo.com.tw>
2015-08-20 09:28:54 +0800

    Add ShellCompletor and its test code

--- sh_emulator.py
+++ sh_emulator.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from collections import defaultdict
 
 
 class ShEmulator:
@@ -18,6 +19,13 @@ class ShEmulator:
             exit(1)
 
     def Run(self):
+        if os.name == "nt":
+            self._WinRun()
+        else:
+            print >>sys.stderr, "UnSupport Platform"
+            exit(1)
+
+    def _WinRun(self):
         while True:
             k = self.getch()
             if k == 224:
@@ -31,10 +39,70 @@ class ShEmulator:
             else:
                 if k == 8 and self.BackspaceHandler:
                     self.BackspaceHandler()
+                elif k == 9 and self.TabHandler:
+                    self.TabHandler()
                 elif k == 13 and self.NewLineHandler:
                     self.NewLineHandler()
                 else:
                     self.OtherKeyHandler(k)
+class ShellCompletor:
+    class Trie:
+        def __init__(self):
+            self.nxt = defaultdict(ShellCompletor.Trie)
+            self.isString = False
+            
+    def __init__(self):
+        self._root = ShellCompletor.Trie()
+
+    @staticmethod
+    def _Go(start, string):
+        return reduce(
+            lambda ac, char: ac.nxt[char],
+            string,
+            start
+        )
+
+#    @staticmethod
+#    def _GoPath(start, string):
+#        '''
+#            :Return:
+#                A path of Trie to string, but no include 'start'
+#        '''
+#        return reduce(
+#            lambda ac, char: (ac[0].nxt[char], ac[1] + ),
+#            string,
+#            (start, [])
+#        )
+
+    @staticmethod
+    def _Traverse(start):
+        results = [""] if start.isString else []
+        for k, v in sorted(start.nxt.items()):
+            results += map(lambda s: k+s, ShellCompletor._Traverse(v))
+        return results
+
+    def Add(self, string):
+        self._Go(self._root, string).isString = True
+
+#    def Delete(self, string):
+#        self
+
+    def Query(self, prefix_string):
+        '''
+            :Return:
+                (complete_chars, candidates)
+                complete_chars          - a string of chars that can complete directly
+                candidates     - a list of possible candidates
+        '''
+        complete_chars = ""
+        pos = self._Go(self._root, prefix_string)
+        while len(pos.nxt) == 1 and not pos.isString:
+            char, pos = pos.nxt.items()[0]
+            complete_chars += char
+        candidates = ShellCompletor._Traverse(pos)
+
+        return (complete_chars, candidates)
+
 class WinCmd:
     def __init__(self):
         def WinBackspaceHanlder():
@@ -45,8 +113,7 @@ class WinCmd:
                 self._inbuf.pop()
 
         def printKey(k):
-            sys.stdout.write(chr(k))
-            self._inbuf.append(chr(k))
+            self._SendString(chr(k))
 
         def WinNewLineHandler():
             plen = len(self._inbuf)
@@ -56,14 +123,38 @@ class WinCmd:
             self._Handle("".join(self._inbuf))
             self._inbuf = []
 
+        def TabHandler():
+            cur_string = "".join(self._inbuf)
+            to_complete, candidates = self._completor.Query(cur_string)
+            if to_complete:
+                self._SendString(to_complete)
+            elif len(candidates) != 1:
+                # show candidates
+                candidates = map(lambda s: cur_string+s, candidates)
+                sys.stdout.write("".join(map(lambda s: chr(10)+s,
+                    ["===="] +
+                    candidates +
+                    [cur_string]
+                )))
+
         self._inbuf = []
+        self._completor = ShellCompletor()
         self.pysh = ShEmulator()
         self.pysh.OtherKeyHandler = printKey
         self.pysh.BackspaceHandler = WinBackspaceHanlder
         self.pysh.NewLineHandler = WinNewLineHandler
+        self.pysh.TabHandler = TabHandler
+
+    def _SendString(self, string):
+        for char in string:
+            sys.stdout.write(char)
+            self._inbuf.append(char)
 
     def Run(self):
         self.pysh.Run()
+
+    def AddComplete(self, cmd):
+        self._completor.Add(cmd)
 
     def _Handle(self, cmd):
 #sys.stdout.writelines(["Hello %s" % cmd])
@@ -71,4 +162,5 @@ class WinCmd:
 
 if __name__ == "__main__":
     cmd = WinCmd()
+    cmd._Handle = lambda s: cmd.AddComplete(s)
     cmd.Run()
